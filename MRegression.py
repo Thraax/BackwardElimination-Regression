f3074d934212bb68719b387c81c6c0775039d868
Yusuf Taha <43391147+Thraax@users.noreply.github.com>
2021-12-02 14:13:27 +0200

    Update MRegression.py

--- MRegression.py
+++ MRegression.py
@@ -1,7 +1,6 @@
 # Import Libraries
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import statsmodels.api as sm
